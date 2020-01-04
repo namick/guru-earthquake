ee1493b3758e6143296ca9973ca466adfbe43fc8
Nathan Amick <github@nathanamick.com>
2020-01-03 20:35:13 -0500

    Filter and limit earthquakes

--- src/index.js
+++ src/index.js
@@ -15,15 +15,23 @@ import "./fonts.scss";
 /**
  * This function should take in earthquake data/current search and returns the sorted, filtered, and sliced list per the assignmen
  */
-export function organizeEarthquakes (search, earthquakes) {
+export function organizeEarthquakes(search, earthquakes) {
+  const filteredEarthquakes = earthquakes.filter(earthquake =>
+    earthquake.place.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return sortByMagnitudeThenTime(filteredEarthquakes).slice(0, 20);
+}
+
+function sortByMagnitudeThenTime(earthquakes) {
   return earthquakes.sort((a, b) => {
     if (a.mag === b.mag) {
-      if (new Date(a.time) < new Date(b.time)) return 1
-      if (new Date(a.time) > new Date(b.time)) return -1
-      return 0
+      if (new Date(a.time) < new Date(b.time)) return 1;
+      if (new Date(a.time) > new Date(b.time)) return -1;
+      return 0;
     }
-    return b.mag - a.mag
-  })
+    return b.mag - a.mag;
+  });
 }
 
 /**
